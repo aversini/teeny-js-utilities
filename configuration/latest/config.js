06a673c3a5059f1328a84fe0aaef214688461d61
Arno Versini <arno.versini@wellsfargo.com>
2021-02-02 17:08:29 -0800

    chore: adding missing dependency for LATEST.md

--- configuration/latest/config.js
+++ configuration/latest/config.js
@@ -2,8 +2,7 @@ const Q = require("q");
 const parserOpts = require("./parser-opts");
 const writerOpts = require("./writer-opts");
 
-module.exports = Q.all([parserOpts, writerOpts]).spread(
-  (parserOpts, writerOpts) => {
-    return { parserOpts, writerOpts };
-  }
-);
+module.exports = Q.all([
+  parserOpts,
+  writerOpts,
+]).spread((parserOpts, writerOpts) => ({ parserOpts, writerOpts }));
